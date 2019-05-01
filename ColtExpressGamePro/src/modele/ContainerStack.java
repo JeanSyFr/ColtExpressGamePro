1e05a29ef2a617b39609542992c4ab813c889d27
Jean Arbache <jean.arbache@u-psud.fr>
2019-05-01 20:58:48 +0200

    Modify the class in order to have different max capacity in sub-classes

--- ColtExpressGamePro/src/modele/ContainerStack.java
+++ ColtExpressGamePro/src/modele/ContainerStack.java
@@ -4,12 +4,12 @@ import java.util.Stack;
 
 public abstract class ContainerStack {
 	private Stack<Butin> butins;
-	private final int  MAX_CAPACITY = 4;
+	private final int  MAX_CAPACITY;
 	private int n ;
-	ContainerStack(){
+	ContainerStack(int maxN){
 		butins = new Stack<Butin>();
 		n= 0;
-		
+		MAX_CAPACITY = maxN;
 	}
 	public void pushButin(Butin b ) {
 		if(n>= this.MAX_CAPACITY) return;

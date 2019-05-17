d43e2f5df43115f2ab4cd08247ced95d020d3f0c
Jean Arbache <jean.arbache@u-psud.fr>
2019-05-17 17:14:06 +0200

    Replace Personne.java manual commit

--- ColtExpressGamePro/src/modele/Personne.java
+++ ColtExpressGamePro/src/modele/Personne.java
@@ -1,29 +1,34 @@
 package modele;
 
-import java.util.Hashtable;
-import java.util.Set;
+import java.util.PriorityQueue;
 
 
-public abstract class Personne {
+public abstract class Personne extends Possesseur{
 	protected String name; // nom de bandit
 	protected Train.Wagon wagon; //Le wagon ou il existe
 	protected ActionList actions; //L'esemble des actions qui va prendre chaque tour max = 5
+	//private static int tour;
 	
-	public String getName() {
-		return this.name;
-	}
 	
 	public Personne(Train t, String name){
-		wagon = personneWagon(t, this); //method de la classe wagon rdv sa propre description
+		super(t.getMAX_N_BUTIN());
+		wagon = mettrePersonneBonWagon(t, this); //method de la classe wagon rdv sa propre description
 		actions = new ActionList(t.getMAX_N_ACTION()); //maximum  actions
 		this.name = name;
+		//tour = 0;
 	}
 	
+	/*public static int getTour() {
+		return tour;
+	}*/
 	
 	//this method will be used in contructor and we will redefine it in each sub-class 
 	//according to polymorphisme, the method applied in the contructor are the good one
-	abstract Train.Wagon personneWagon(Train t, Personne p); // return the wagon where p should be 
+	abstract Train.Wagon mettrePersonneBonWagon(Train t, Personne p); // return the wagon where p should be 
 	abstract void executeAction(); 	//execute le premiere action s'il en a	
+	
+	
+	
 	public void addAction(Action a) {
 		actions.addAction(a);
 	}
@@ -36,47 +41,41 @@ public abstract class Personne {
 		System.out.println(this.name + " has shot "+b2.name);
 
 			
-		if(!b2.sac.isEmpty())
-			wagon.addButin(b2.sac.popButin());
+		if(!b2.isEmpty())
+			wagon.addButin(b2.popButin());
 	}
 	
+	/*protected static void incrementTour() {
+		tour++;
+	}
+	protected static void decrementTour() {
+		tour--;
+	}*/
 	
 	
 	
 	
 	
 	protected class ActionList {
-		private Hashtable<Integer, Action> actions;
+		/*
+		 * A modifier pour faire une file
+		 */
+		private PriorityQueue<Action> actions;
 		private int MAX_N;
 		private int index;
 		ActionList(int n){
 			MAX_N = n;
 			index=0;
-			actions = new Hashtable<Integer, Action>();
+			actions = new PriorityQueue<Action>();
 		}
 		protected void addAction(Action act) {
 			if(index>= MAX_N) return;
-			this.actions.put(index, act);
-			index++;
+			this.actions.add( act);
 		}
-		private int minSet(Set<Integer> s) {
-			int out = this.MAX_N;
-			for(int k : s) {
-				if(out>k) out = k;
-			}
-			return out;
-		}
-		
 		
 		protected Action actionToExecute() {
-			if(actions.size()==0) return null;
-			int firstActionIndex = minSet(actions.keySet());
-			Action out = actions.get(firstActionIndex);
-			actions.remove(firstActionIndex);
-			return out;
+			return actions.poll();
 		}
-		
-		
 		
 	}
 	
